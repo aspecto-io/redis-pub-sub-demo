bdbfbee71f8529ec278e05a58dcff39a7724a859
Michael Haberman <michael@aspecto.io>
2021-03-28 14:29:08 +0300

    no consume

--- src/service-subscriber.ts
+++ src/service-subscriber.ts
@@ -5,8 +5,7 @@ import * as Redis from "ioredis";
 const redis = new Redis({
 });
 redis.on('message', (channel, message) => {
-    const root = propagation.extract(context.active(), JSON.parse(message));
-    const consume = otel.startSpan('consume redis message!', {attributes:{channel, message}}, root);
+    const consume = otel.startSpan('consume redis message!', {attributes:{channel, message}});
 
     console.log('message', { channel, message, traceId: consume.context().traceId })
 
