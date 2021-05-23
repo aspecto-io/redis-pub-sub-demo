1c66ae3e7c9b21f942b96df1aadcc3ffcd866283
michael haberman <michaelhaberman@michaels-MacBook-Pro.local>
2021-05-23 14:12:18 +0300

    with consumer

--- src/service-subscriber.ts
+++ src/service-subscriber.ts
@@ -5,7 +5,8 @@ import * as Redis from "ioredis";
 const redis = new Redis({
 });
 redis.on('message', (channel, message) => {
-    const consume = otel.startSpan('consume redis message!', {attributes:{channel, message}});
+    const root = propagation.extract(context.active(), JSON.parse(message));
+    const consume = otel.startSpan('consume redis message!', {attributes:{channel, message}}, root);
 
     console.log('message', { channel, message, traceId: consume.context().traceId })
 
